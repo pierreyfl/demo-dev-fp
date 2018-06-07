043bf71d651965ecb227dd6680b3aca02f739fa0
Pierre Yam <pierreyam@icloud.com>
2018-06-07 23:43:54 +0800

    Add project timeline

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,11 @@
 module ProjectsHelper
+  def timeline_display(project)
+    if project.end_date.to_date < Date.today
+      days = (Date.today - project.end_date.to_date).to_i
+      "#{days} #{'day'.pluralize(days)} overdue"
+    elsif project.start_date.to_date < Date.today
+      days = (Date.today - project.start_date.to_date).to_i
+      "day #{days}"
+    end
+  end
 end
